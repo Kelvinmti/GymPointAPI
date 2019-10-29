beec7160fab752994df55138cbb3d44e582daf81
Kelvinmti <kdevweb@gmail.com>
2019-10-29 17:47:02 -0300

    Concluido cadastrar usuarios

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -5,33 +5,33 @@ import Student from '../models/Student';
 class StudentController {
   async store(req, res) {
     const schema = Yup.object().shape({
-      nome: Yup.string()
-        .required()
-        .typeError('Campo Nome é obrigatório'),
+      nome: Yup.string().required('Campo nome é obrigatório'),
       email: Yup.string()
-        .email()
-        .required()
-        .typeError('Campo Email é obrigatório'),
+        .email('Informe um email válido')
+        .required('Campo email é obrigatório'),
       idade: Yup.number()
-        .required()
-        .positive()
-        .integer()
-        .typeError('Campo Email é obrigatório'),
-      peso: Yup.number().required(),
-      altura: Yup.number().required(),
+        .required('Campo idade é obrigatório')
+        .positive('Campo idade deve ser um número maior que zero')
+        .integer(),
+      peso: Yup.number()
+        .positive('Campo peso deve ser um número maior que zero')
+        .required('Campo peso é obrigatório'),
+      altura: Yup.number()
+        .positive('Campo altura deve ser um número maior que zero')
+        .required('Campo altura é obrigatório')
+        .typeError('Campo altura deve ser um número'),
     });
 
     const errosValidacao = [];
 
-    schema.validate(req.body).catch(err => {
-      errosValidacao;
+    await schema.validate(req.body).catch(err => {
+      errosValidacao.push(err.errors);
     });
 
     if (errosValidacao.length > 0) {
-      return res.status(400).json({
-        erros: err.errors,
-      });
+      return res.status(400).json(errosValidacao);
     }
+
     // if (!(await schema.isValid(req.body))) {
     //   return res.status(400).json({
     //     error: 'Campos obrigatórios inválidos ou não preenchidos corretamente.',
--- src/database/migrations/20191029204130-create-students.js
+++ src/database/migrations/20191029204130-create-students.js
@@ -28,6 +28,14 @@ module.exports = {
         type: Sequelize.DECIMAL(3, 2),
         allowNull: false,
       },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
     });
   },
 
