9f62039da58f557d2a4b86eca9cf157aaf54f665
Kelvin Mendes <kdevweb@gmail.com>
2019-10-31 23:19:37 -0300

    Finishing create Enrollment

--- src/app/controllers/EnrollmentController.js
+++ src/app/controllers/EnrollmentController.js
@@ -7,35 +7,36 @@ import Student from '../models/Student';
 
 class EnrollmentController {
   async store(req, res) {
+
     const schema = Yup.object().shape({
       student_id: Yup.number()
-        .required('student_id field is mandatory.')
-        .positive('student_id field must be a number higher than zero.')
-        .integer('student_id field must be an integer.')
-        .typeError('student_id field must be a number.'),
+        .required('student field is mandatory.')
+        .positive('student field must be a number higher than zero.')
+        .integer('student field must be an integer.')
+        .typeError('student field must be a number.'),
       plan_id: Yup.number()
-        .required('plan_id field is mandatory.')
-        .positive('plan_id field must be a number higher than zero.')
-        .integer('plan_id field must be an integer.')
-        .typeError('plan_id field must be a number.'),
+        .required('plan field is mandatory.')
+        .positive('plan field must be a number higher than zero.')
+        .integer('plan field must be an integer.')
+        .typeError('plan field must be a number.'),
       start_date: Yup.date()
-        .required('start_date field is mandatory.')
-        .typeError('start_date field must be a valid date.'),
+        .required('start date field is mandatory.')
+        .typeError('start date field must be a valid date.'),
     });
 
     await schema.validate(req.body).catch(err => {
       return res.status(400).json({ error: err.errors });
     });
-
+    
     const { student_id, plan_id, start_date } = req.body;
 
-    const plan = Plan.findByPk(plan_id);
-
+    const plan = await Plan.findByPk(plan_id);
+    
     if (!plan) {
       return res.status(400).json({ error: 'Plan not found.' });
     }
 
-    const student = Student.findByPk(student_id);
+    const student = await Student.findByPk(student_id);
     if (!student) {
       return res.status(400).json({ error: 'Student not found.' });
     }
@@ -50,13 +51,27 @@ class EnrollmentController {
 
     const total_price = plan.price * plan.duration;
 
-    res.json({
-      student_id,
-      plan_id,
-      start_date_formated,
-      end_date,
-      total_price,
-    });
+    try {
+      const { created_at } = await Enrollment.create({
+        student_id,
+        plan_id,
+        start_date: start_date_formated,
+        end_date,
+        price: total_price
+      });
+  
+      res.json({
+        student_id,
+        plan_id,
+        start_date_formated,
+        end_date,
+        total_price,
+        created_at
+      });      
+    } catch (error) {
+      return res.status(400).json({ error });
+    }
+    
   }
 }
 
--- src/app/models/Enrollment.js
+++ src/app/models/Enrollment.js
@@ -4,6 +4,8 @@ class Enrollment extends Model {
   static init(sequelize) {
     super.init(
       {
+        student_id: Sequelize.INTEGER,
+        plan_id: Sequelize.INTEGER,
         start_date: Sequelize.DATE,
         end_date: Sequelize.DATE,
         price: Sequelize.DECIMAL(7, 2),
@@ -17,8 +19,8 @@ class Enrollment extends Model {
   }
 
   static associate(models) {
-    this.belongsTo(models.Student, { foreignKey: 'student_id', as: 'student' });
-    this.belongsTo(models.Plan, { foreignKey: 'plan_id', as: 'plan' });
+    this.belongsTo(models.Student, { foreignKey: 'id', as: 'student' });
+    this.belongsTo(models.Plan, { foreignKey: 'id', as: 'plan' });
   }
 }
 
