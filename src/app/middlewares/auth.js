cae187850c92e61b97ca92c009f46275dee5af05
Kelvin Mendes <kdevweb@gmail.com>
2019-11-06 23:54:25 -0300

    Finishing email

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -21,6 +21,10 @@ export default async (req, res, next) => {
 
     return next();
   } catch (error) {
-      return res.status(401).json({ error: 'Invalid Token.' });    
+    console.log(error);
+    if (error.name == 'TokenExpiredError')
+      return res.status(401).json({ error: 'Expired Token.' });
+    
+    return res.status(401).json({ error: 'Invalid Token.' });    
   }
 };
